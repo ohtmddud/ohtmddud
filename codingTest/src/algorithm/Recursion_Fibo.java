05fe087f189ee4597d638394cc801d147aeb8fb2
Oh Seung Young <ohsy94@gmail.com>
2022-12-11 22:04:28 +0900

    1. memoization 적용

--- codingTest/src/algorithm/Recursion_Fibo.java
+++ codingTest/src/algorithm/Recursion_Fibo.java
@@ -5,6 +5,11 @@ public class Recursion_Fibo {
 	public static void main(String[] args) {
 //		피보나치 수열 연습
 		System.out.println(Fibonacci(4));
+
+		fib = new int[10];
+		for (int n = 1; n < 10; n++) {
+			System.out.print(fibo(n) + " ");
+		}
 	}
 
 	// 1 1 2 3 5 8 13..
@@ -16,6 +21,24 @@ public class Recursion_Fibo {
 		}
 //		2. Recursive calls
 		return Fibonacci(n - 2) + Fibonacci(n - 1);
+	}
+
+	
+	static int[] fib;
+	static int fibo(int n) {
+		
+		// memoization
+		if (fib[n] > 0) {
+			return fib[n];
+			
+		} else if (n == 1) {
+			return fib[1] = 1;
+			
+		} else if (n == 2) {
+			return fib[2] = 1;
+		}
+		
+		return fib[n] = fibo(n - 2) + fibo(n - 1);
 	}
 
 }
