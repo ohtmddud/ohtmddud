78fdf551a12fa5a7f124d8f361bb296f1ac92fad
오승영 <ohsy94@gmail.com>
2023-02-17 00:56:40 +0900

    이해한 대로 따라서 코드를 구현해봤는데 결과가 다르게 나옴. 이유를 찾는 중.

--- codingTest/src/codingTest/pro_3190.java
+++ codingTest/src/codingTest/pro_3190.java
@@ -8,6 +8,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
+//의사코드
+//while(true){
+//	  1. 시간재기
+//	  2. 뱀 이동하기 
+//	  3. 범위를 벗어나거나, 뱀 몸통 만날 때 종료
+//	  4. 사과가 있을 때 없을 때 처리 
+//	  5. 방향을 바꿔주는 시간을 만날 때 방향 변경 
+//	  6. 현재값 업데이트 
+//	}
+
 public class pro_3190 {
 	// 백준 3190 뱀(dummy) (https://www.acmicpc.net/problem/3190)
 	private static int N, apple, L;
@@ -26,8 +36,8 @@ public class pro_3190 {
 		apple = Integer.parseInt(br.readLine());
 		for(int i = 0; i < apple; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken()) -1;
+			int y = Integer.parseInt(st.nextToken()) -1;
 			map[x][y] = 1;
 		}
 		L = Integer.parseInt(br.readLine());
@@ -37,9 +47,76 @@ public class pro_3190 {
 			String y = st.nextToken();
 			hm.put(x, y);
 		}
-		
 		System.out.println(N);
 		System.out.println(apple);
 		System.out.println(L);
+		
+		solve();
+	}
+	
+	public static boolean finish(int x, int y) {
+		if(x<0 || y<0 || x>=N || y>=N) {
+			return true;
+		}
+		for(int i=0; i<snake.size(); i++) {
+			int[] t = snake.get(i);
+			if(x==t[0] && y==t[1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void solve() {
+		int x = 0, y = 0;
+		int time = 0;
+		int d = 0;
+		snake.add(new int[] {0, 0});
+		
+		while(true) {
+			// 시간
+			time++; 
+			
+			// 이동
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			// 범위 밖, 몸통 종료
+			if(finish(nx, ny)) {
+				System.out.println(nx + ", " + ny);
+				break;
+			}
+			
+			// 사과
+			if(map[nx][ny] == 1) {
+				map[nx][ny] = 0;
+				snake.add(new int[] {nx, ny});
+			} else {
+				snake.add(new int[] {nx, ny});
+				snake.remove(0);
+			}
+			
+			// 방향 전환
+			if(hm.containsKey(time)) {
+				if(hm.get(time).equals("D")) {
+					System.out.println(hm.get(time));
+					d += 1;
+					if(d==4) {
+						d=0;
+					}else {
+						d -= 1;
+						if(d==-1) {
+							d=3;
+						}
+					}
+				}
+			}
+			
+			// 업데이트
+			x = nx;
+			y = ny;
+			
+		}
+		System.out.println(time);
 	}
 }
