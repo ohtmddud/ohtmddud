06b2e57965e580e5a679adf53d75af5ab3e0584e
오승영 <ohsy94@gmail.com>
2023-02-22 02:24:49 +0900

    주사위 굴리기 코딩테스트 문제 풀이중

---
+++ codingTest/src/codingTest/pro_14499.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pro_14499 {
+	static int N, M, x, y, K;
+	static int[] n, k;
+	
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<N; i++) {
+			
+			
+		}
+		for(int i=0; i<K; i++) {
+			
+		}
+
+	}
+
+}
