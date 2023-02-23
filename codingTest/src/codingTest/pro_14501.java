6ce74096115bce2c0077008c8ebcd0806c969ef4
오승영 <ohsy94@gmail.com>
2023-02-23 21:08:04 +0900

    퇴사 구현한 방식의 DP 배열은 그 날짜부터 시작했을 때의 최댓값이다.
    즉, DP[4]는 4일부터 N일까지 최댓값.
    
    우리가 원하는 것은 1일부터이므로 DP[1]의 값을 구해주면 된다.
    
    DP[i] + T[i]가 N+1보다 크다면, i 날짜에 시작하는 일을 할 수 없다는 뜻이다.
    
    때문에, 무조건 DP[i]의 값은 DP[i+1]이 되어야한다.
    
    i 날짜에 i의 일을 하지 못한다면 i+1 날짜부터 시작한 최댓값과 i 날짜에 시작한 최댓값이 같기 때문이다.
    
    DP[i] + T[i]가 N+1과 작거나 같다면, i 날짜에 시작하는 일을 할 수 있다는 뜻이다.
    
    하지만, i날짜의 일을 하는 것이 최댓값을 보장해준다고 할 수 없다.
    
    때문에, DP[i+1]과 P[i] + DP[i+T[i]]의 값 중 최댓값이 DP[i]가 되어야한다.
    
    i 날짜의 일을 하면, i의 금액과 i일을 하고 난 후, DP[i+T[i]]의 값을 벌 수 있고,
    
    하지 않는다면, DP[i+1]의 값을 벌 수 있기 때문이다.

---
+++ codingTest/src/codingTest/pro_14501.java
@@ -0,0 +1,39 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pro_14501 {
+	// 퇴사 https://www.acmicpc.net/problem/14501
+	private static int N;
+	private static int[] T, P;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		T = new int[N+1];
+		P = new int[N+1];
+		int[] DP = new int[N+2];
+		
+		StringTokenizer st;
+		for(int i=1; i<N+1; i++) {
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());  
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(N);
+		for(int i=N; i>0; i--) {
+			if(i + T[i] > N + 1) {
+				DP[i] = DP[i+1];
+			} else {
+				DP[i] = Math.max(DP[i+1], P[i] + DP[i+T[i]]);
+			}
+		}
+		
+		System.out.println(DP[1]);
+
+	}
+
+}
