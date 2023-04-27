4ebe0c7d11ef09f2bba17cceeef5dd723d23bd59
sy <sy@211.63.210.153>
2023-04-27 22:46:54 +0900

    Top down 방식으로 풀 때, integer로 만들어서 초기값을 null 로 설정 점화식을 세우는게 중요하다

--- codingTest/src/algorithm/P12865.java
+++ codingTest/src/algorithm/P12865.java
@@ -8,6 +8,8 @@ import java.util.StringTokenizer;
 public class P12865 {
 	// https://www.acmicpc.net/problem/12865, 평범한 배낭
 	// https://st-lab.tistory.com/141 (해설)
+	private static int[] W, V;
+	private static Integer[][] A;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,26 +18,39 @@ public class P12865 {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[] W = new int[N + 1];
-		int[] V = new int[N + 1];
-		int[] dp = new int[K + 1];
-//		int[][] dp = new int[N][K+1]; 
-				
+		W = new int[N + 1];
+		V = new int[N + 1];
+//		int[] A = new int[K + 1];
+		A = new Integer[N][K + 1];
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			W[i] = Integer.parseInt(st.nextToken());
 			V[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
+		System.out.println(knapsack(N, K));
 		// Bottom-Up 1
-		
+
 		// Bottom-Up 2
-		
+
 	}
+
 	// Top-down
 	static int knapsack(int i, int k) {
-		return 0;
+		if (i < 0) {
+			return 0;
+		}
+
+		if (A[i][k] == null) {
+
+			if (W[i] < k) {
+				A[i][k] = knapsack(i - 1, k);
+			} else {
+				A[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+			}
+		}
+		return A[i][k];
 	}
-	
 
 }
